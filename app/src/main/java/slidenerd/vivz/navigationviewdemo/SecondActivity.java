6ad79dc0911494b08da98897c7fdef15500888cc
slidenerd <slidenerd@gmail.com>
2015-06-23 19:15:53 +0530

    and thats how you use tablayout with viewpager

--- app/src/main/java/slidenerd/vivz/navigationviewdemo/SecondActivity.java
+++ app/src/main/java/slidenerd/vivz/navigationviewdemo/SecondActivity.java
@@ -1,17 +1,51 @@
 package slidenerd.vivz.navigationviewdemo;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
 
 
 public class SecondActivity extends ActionBarActivity {
+
+    private RelativeLayout mRoot;
+    private TextInputLayout mEmailLayout;
+    private TextInputLayout mPasswordLayout;
+    private EditText mInputEmail;
+    private EditText mInputPassword;
+    private FloatingActionButton mFAB;
+    private View.OnClickListener mSnackBarClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
+    private View.OnClickListener mFabClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Snackbar.make(mRoot, "FAB Clicked", Snackbar.LENGTH_SHORT)
+                    .show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        mRoot = (RelativeLayout) findViewById(R.id.root_activity_second);
+        mEmailLayout = (TextInputLayout) findViewById(R.id.email_layout);
+        mPasswordLayout = (TextInputLayout) findViewById(R.id.password_layout);
+        mInputEmail = (EditText) findViewById(R.id.input_email);
+        mInputPassword = (EditText) findViewById(R.id.input_password);
+        mFAB = (FloatingActionButton) findViewById(R.id.fab);
+        mFAB.setOnClickListener(mFabClickListener);
+
     }
 
     @Override
@@ -34,5 +68,38 @@ public class SecondActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void submit(View view) {
+
+        boolean isEmptyEmail = isEmptyEmail();
+        boolean isEmptyPassword = isEmptyPassword();
+        if (isEmptyEmail && isEmptyPassword) {
+            Snackbar.make(mRoot, "One Or More Fields Are Blank", Snackbar.LENGTH_SHORT)
+                    .setAction(getString(R.string.text_dismiss), mSnackBarClickListener)
+                    .show();
+        } else if (isEmptyEmail && !isEmptyPassword) {
+            mEmailLayout.setError("Email Cannot Be Empty");
+            mPasswordLayout.setError(null);
+        } else if (!isEmptyEmail && isEmptyPassword) {
+            mPasswordLayout.setError("Password Cannot Be Empty");
+            mEmailLayout.setError(null);
+        } else {
+            //All Good Here
+        }
+    }
+
+    private boolean isEmptyEmail() {
+        return mInputEmail.getText() == null
+                || mInputEmail.getText().toString() == null
+                || mInputEmail.getText().toString().isEmpty();
+
+    }
+
+    private boolean isEmptyPassword() {
+        return mInputPassword.getText() == null
+                || mInputPassword.getText().toString() == null
+                || mInputPassword.getText().toString().isEmpty();
+
     }
 }
--- app/src/main/java/slidenerd/vivz/navigationviewdemo/ThirdActivity.java
+++ app/src/main/java/slidenerd/vivz/navigationviewdemo/ThirdActivity.java
@@ -1,17 +1,45 @@
 package slidenerd.vivz.navigationviewdemo;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 
-public class ThirdActivity extends ActionBarActivity {
+public class ThirdActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private TabLayout mTabLayout;
+    private ViewPager mPager;
+    private MyPagerAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+        mAdapter = new MyPagerAdapter(getSupportFragmentManager());
+        mToolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(mToolbar);
+        mTabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(mAdapter);
+        mTabLayout.setTabsFromPagerAdapter(mAdapter);
+
+        mTabLayout.setupWithViewPager(mPager);
+        mPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+
+
     }
 
     @Override
@@ -35,4 +63,54 @@ public class ThirdActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public static class MyFragment extends Fragment {
+        public static final java.lang.String ARG_PAGE = "arg_page";
+
+        public MyFragment() {
+
+        }
+
+        public static MyFragment newInstance(int pageNumber) {
+            MyFragment myFragment = new MyFragment();
+            Bundle arguments = new Bundle();
+            arguments.putInt(ARG_PAGE, pageNumber + 1);
+            myFragment.setArguments(arguments);
+            return myFragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            Bundle arguments = getArguments();
+            int pageNumber = arguments.getInt(ARG_PAGE);
+            TextView myText = new TextView(getActivity());
+            myText.setText("Hello I am the text inside this Fragment " + pageNumber);
+            myText.setGravity(Gravity.CENTER);
+            return myText;
+        }
+    }
 }
+
+class MyPagerAdapter extends FragmentStatePagerAdapter {
+
+    public MyPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        ThirdActivity.MyFragment myFragment = ThirdActivity.MyFragment.newInstance(position);
+        return myFragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 10;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return "Tab " + (position + 1);
+    }
+}
+
